ffc03f5c2d2603fc9b734c7dce7640608bc7357f
Mohammad Khaledur Rahman <morahma@v-017.victor.futuresystems.org>
2020-09-16 04:45:51 -0400

    added f2v force computations using dgl kernels

--- examples/pytorch/force2vec/force2vec.py
+++ examples/pytorch/force2vec/force2vec.py
@@ -4,6 +4,16 @@ from dgl.data.citation_graph import load_cora
 import sys, time
 import numpy as np
 
+def cacheflush():
+	print("Cache Refreshing...")
+	BIGNUM = 100000
+	L1 = []
+	L2 = []
+	for i in range(BIGNUM):
+		L1.insert(0, i)
+		L2.insert(0, i+1)
+	print("Done!")
+	
 def batch_process(graph, batchsize=64):
 	batches = []
 	b = 0
@@ -20,6 +30,7 @@ def batch_process(graph, batchsize=64):
 		b += batchsize
 	return batches
 
+#combined sddmmspmm kernel
 def f2valgo(batchgraphs, embed, iterations = 5):
 	it = 0
 	#remember to add learning rate ...
@@ -30,12 +41,13 @@ def f2valgo(batchgraphs, embed, iterations = 5):
 		end = time.time()
 		totalktime += end - start
 		it += 1
-	print("Total Kernel Time:", totalktime, "seconds")
+	print("Total F2V Kernel Time:", totalktime, "seconds")
 	return embed
 
-def f2vusingdefault(batchgraphs, embed, iterations=5):
+#gdl:sddmm+transformation+spmm kernel
+def f2vusingdefault(batchgraphs, batchgraphsT, embed, iterations=5):
 	it = 0
-	totaltime = 0
+	totalktime = 0
 	while it < iterations:
 		start = time.time()
 		#SDDMM operation
@@ -43,12 +55,14 @@ def f2vusingdefault(batchgraphs, embed, iterations=5):
 		#non-linear transformation
 		Y = 1. / (1 + np.exp(-X))
 		#SPMM operation
+		output = _gspmm(batchgraphsT._graph, "mul", "sum", embed, Y)[0]
 		end = time.time()
 		totalktime += end - start
 		it += 1
-	print("Total Kernel Time:", totalktime, "seconds")
+	print("Total GDL Kernel Time:", totalktime, "seconds")
 	return embed
 
+#manual result verification
 def gsddmmspmmkerneltest(graph, A):
 	coo = graph.adj()._indices().numpy()
 	N = graph.adj().size()[0]
@@ -73,26 +87,51 @@ def gsddmmspmmkerneltest(graph, A):
 	O = np.matmul(Z, A)
 	return O
 
+def transposeGraph(graph):
+	edges = graph.edges()
+	source = edges[0]
+	dest = edges[1]
+	tGraph = dgl.graph((dest, source))
+	return tGraph
+
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
 		data = load_cora(".")
 		graph = data[0]
+		tgraph = transposeGraph(graph)
 		N = len(graph.nodes())
-		embed = torch.rand(N, N)
+		embed = torch.rand(N, 128)
 		#need to check batch processing ...
-		bgraphs = batch_process(graph)
+		#bgraphs = batch_process(graph)
 		#print(graph)
+		cacheflush()
+		#output = f2valgo(graph, embed)
+		#print("SDDMMSPMM Kernel:")
+		#print(output)
+		#cacheflush()
+		dgloutput = f2vusingdefault(graph, tgraph, embed)
+		print("DGL: SDDMMSPMM+Transformation+SPMM")
+		print(dgloutput)
+		#cacheflush()
 		#f2valgo(graph, embed)
 	else:
 		print("Simple SDDMMSPMM Test:")
 		g = dgl.graph(([0, 0, 1, 1, 2, 3], [1, 2, 2, 4, 3, 4]))
+		gt = dgl.graph(([1, 2, 2, 4, 3, 4], [0, 0, 1, 1, 2, 3]))
 		#g = dgl.graph(([0, 1, 2, 3], [1, 2, 3, 4]))
 		embed = torch.rand(5, 5)
 		#print(embed)
-		output = _gsddmmspmm(g._graph, "mul", embed, embed, "u", "v")
-		print("SPDDMMSPMM Kernel:")
+		#output = _gsddmmspmm(g._graph, "mul", embed, embed, "u", "v")
+		cacheflush()
+		output = f2valgo(g, embed)
+		print("SDDMMSPMM Kernel:")
 		print(output)
-		print("Manual SPDDMM+SPMM:")
+		cacheflush()
+		dgloutput = f2vusingdefault(g, gt, embed) 
+		print("DGL: SDDMMSPMM+Transformation+SPMM")
+		print(dgloutput)
+		#output = f2valgo(g, embed)
+		#print("Manual SPDDMM+SPMM:")
 		#print(embed)
-		mout = gsddmmspmmkerneltest(g, embed)
-		print(mout)
+		#mout = gsddmmspmmkerneltest(g, embed)
+		#print(mout)
