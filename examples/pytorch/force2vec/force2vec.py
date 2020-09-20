655f210c801354e1e690670a676094247122fbe6
Mohammad Khaledur Rahman <morahma@v-login1.victor.futuresystems.org>
2020-09-20 14:50:49 -0400

    updated f2v with all components

--- examples/pytorch/force2vec/force2vec.py
+++ examples/pytorch/force2vec/force2vec.py
@@ -1,7 +1,7 @@
 import dgl,torch
 from dgl.sparse import _gsddmmspmm, _gsddmm, _gspmm
 from dgl.data.citation_graph import load_cora, load_citeseer, load_pubmed
-import sys, time
+import sys, time, random
 import numpy as np
 from math import log, exp
 from scipy.io import mmread,mminfo
@@ -17,8 +17,35 @@ def cacheflush():
 		L1.insert(0, i)
 		L2.insert(0, i+1)
 	print("Done!")
+
+SM_BOUND = 5.0
+SM_TABLE_SIZE = 2048
+SM_RESOLUTION = SM_TABLE_SIZE / (2.0 * SM_BOUND)
+
+#create sigmoid table to make it equivalent
+def init_SM_TABLE():
+	sm_table = [0.0 for i in range(SM_TABLE_SIZE)]
+	for i in range(SM_TABLE_SIZE):
+		x = 2.0 * SM_BOUND * i / SM_TABLE_SIZE - SM_BOUND
+		sm_table[i] = 1.0 / (1.0 + exp(-x))
+	return sm_table
+
+sm_table = init_SM_TABLE()
+
+def FastSigmoid(v, stable):
+	if v > SM_BOUND:
+		return 1.0
+	elif v < -SM_BOUND:
+		return 0.0
+	return stable[int((v + SM_BOUND) * SM_RESOLUTION)]
+
+def allFastSigmoid(T, stable, add = 1.0):
+	for i in range(len(T)):
+		T[i] = add - FastSigmoid(T[i], stable)
+	return T
+
 	
-def batch_process(graph, batchsize=64):
+def batch_process(graph, batchsize=256):
 	batches = []
 	b = 0
 	N = len(graph.nodes())
@@ -30,22 +57,58 @@ def batch_process(graph, batchsize=64):
 		bedge_left = [int(i) for [i,j] in bedges]
 		bedge_right = [int(j) for [i,j] in bedges]
 		bgraph = dgl.graph((bedge_left,bedge_right))
-		batches.append([bgraph, b , min(b+batchsize, N)])
+		
+		#discard subgraph with no edge
+		if len(bgraph.edges()[0]) > 0:
+			batches.append([bgraph, b , min(b+batchsize, N)])
+		
 		#batches.append(bgraph)
 		b += batchsize
 	return batches
 
+def negativeSamples(graph, start, end, nsamples = 2):
+	random.seed(1)
+	left = []
+	right = []
+	N = len(graph.nodes())
+	for i in range(start, end):
+		for j in range(nsamples):
+			left.append(i)
+			v = random.randint(0, N-1)
+			right.append(v)
+	ngraph = dgl.graph((left, right))
+	return ngraph
+
+#commonRepulsive function
+def calcRepulsive(graph, embed, sm_table, V):
+	X = _gsddmm(graph._graph, 'dot', embed[:V], embed[:V], lhs_target='u', rhs_target='v')
+	Y = allFastSigmoid(X, sm_table, 0)
+	output = _gspmm(graph._graph.reverse(), "mul", "sum", embed[:V], Y)[0]
+	return output
+
+#update function
+def update(s, e, embed, upembed):
+	for i in range(s, e):
+		for j in range(len(embed[i])):
+			embed[i][j] = upembed[i][j].clone()
+	return embed
 
 #combined sddmmspmm kernel
 def f2vusingsigmoid(batchgraphs, embed, iterations = 1, lrate=1.0):
 	it = 0
-	#remember to add learning rate ...
+	#for repulsive force
+	#sm_table = init_SM_TABLE()
 	totalktime = 0
 	while it < iterations:
 		start = time.time()
 		for [graph, s, e] in batchgraphs:
-			output = _gsddmmspmm(graph._graph, "mul", embed, embed, "u", "v", 1)
-			embed = embed + lrate * output
+			ngraph = negativeSamples(graph, s, e)
+			nV = len(ngraph.nodes())
+			#print(ngraph, ngraph.edges())
+			outputa = _gsddmmspmm(graph._graph, "mul", embed, embed, "u", "v", 1)
+			outputr = calcRepulsive(ngraph, embed, sm_table, nV)
+			embed[s:e] = lrate * outputa[s:e].clone()
+			embed[s:e] = embed[s:e] + lrate * outputr[s:e]
 		end = time.time()
 		totalktime += end - start
 		it += 1
@@ -57,38 +120,15 @@ def f2vusingtdistribution(batchgraphs, embed, iterations = 1, lrate=1.0):
 	totalktime = 0
 	while it < iterations:
 		start = time.time()
-		output = _gsddmmspmm(batchgraphs._graph, "mul", embed, embed, "u", "v", 2)
+		for [graph, s, e] in batchgraphs:
+			outputa = _gsddmmspmm(graph._graph, "mul", embed, embed, "u", "v", 2)
+			embed = embed + lrate * outputa
 		end = time.time()
 		totalktime += end - start
 		it += 1
 	print("Total F2V Kernel Time:", totalktime, "seconds")
-	return output
+	return embed
 	
-SM_BOUND = 5.0
-SM_TABLE_SIZE = 2048
-SM_RESOLUTION = SM_TABLE_SIZE / (2.0 * SM_BOUND)
-
-
-#create sigmoid table to make it equivalent
-def init_SM_TABLE():
-	sm_table = [0.0 for i in range(SM_TABLE_SIZE)]
-	for i in range(SM_TABLE_SIZE):
-		x = 2.0 * SM_BOUND * i / SM_TABLE_SIZE - SM_BOUND
-		sm_table[i] = 1.0 / (1.0 + exp(-x))
-	return sm_table
-
-def FastSigmoid(v, stable):
-	if v > SM_BOUND:
-		return 1.0
-	elif v < -SM_BOUND:
-		return 0.0
-	return stable[int((v + SM_BOUND) * SM_RESOLUTION)]
-
-def allFastSigmoid(T, stable):
-	for i in range(len(T)):
-		T[i] = 1.0 - FastSigmoid(T[i], stable)
-	return T
-
 def scale(v):
 	if v > SM_BOUND:
 		return SM_BOUND
@@ -110,16 +150,21 @@ def dglusingsigmoid(batchgraphs, embed, iterations=1, lrate=1.0):
 	#print(sm_table)
 	while it < iterations:
 		start = time.time()
-		for [graph, s, e] in batchgraphs: 
-			numnodes = len(graph.nodes())
+		for [graph, s, e] in batchgraphs:
+			ngraph = negativeSamples(graph, s, e)
+			nV = len(ngraph.nodes())
+			#just to make sure: dim(sparse graph) == dim(embedding)
+			pV = len(graph.nodes())
 			#SDDMM operation
-			X = _gsddmm(graph._graph, 'dot', embed[:numnodes], embed[:numnodes], lhs_target='u', rhs_target='v')
+			X = _gsddmm(graph._graph, 'dot', embed[:pV], embed[:pV], lhs_target='u', rhs_target='v')
 			#non-linear transformation
 			#Y = 1.0 - 1.0 / (1 + torch.exp(-X))
 			Y = allFastSigmoid(X, sm_table)
 			#SPMM operation
-			output = _gspmm(graph._graph.reverse(), "mul", "sum", embed[:numnodes], Y)[0]
-			embed[:numnodes] = embed[:numnodes] + lrate * output
+			outputa = _gspmm(graph._graph.reverse(), "mul", "sum", embed[:pV], Y)[0]
+			outputr = calcRepulsive(ngraph, embed, sm_table, nV)
+			embed[s:e] = lrate * outputa[s:e]
+			embed[s:e] = embed[s:e] + lrate * outputr[s:e]
 		end = time.time()
 		totalktime += end - start
 		it += 1
@@ -131,25 +176,28 @@ def dglusingtdistribution(batchgraphs, embed, iterations=1, lrate = 1.0):
 	totalktime = 0
 	while it < iterations:
 		start = time.time()
-		#SDDMM
-		D = _gsddmm(batchgraphs._graph, "sub", embed, embed, lhs_target='u', rhs_target='v')
-		d = _gsddmm(batchgraphs._graph, 'dot', D, D, lhs_target='e', rhs_target='e')
-		#NonlinearTransformation
-		E = - 2.0 / (1.0 + d)
-		#SDDMM
-		E = _gsddmm(batchgraphs._graph, "mul", D, E, lhs_target='e', rhs_target='e')
-		#scaling
-		E = allscale(E)
-		#SPMM
-		output = _gspmm(batchgraphs._graph.reverse(), "copy_rhs", "sum", E, E)[0]
+		for [graph, s, e] in batchgraphs:
+			#SDDMM
+			D = _gsddmm(graph._graph, "sub", embed, embed, lhs_target='u', rhs_target='v')
+			d = _gsddmm(graph._graph, 'dot', D, D, lhs_target='e', rhs_target='e')
+			#NonlinearTransformation
+			E = - 2.0 / (1.0 + d)
+			#SDDMM
+			E = _gsddmm(graph._graph, "mul", D, E, lhs_target='e', rhs_target='e')
+			#scaling
+			E = allscale(E)
+			#SPMM
+			outputa = _gspmm(graph._graph.reverse(), "copy_rhs", "sum", E, E)[0]
+			embed = embed + lrate * outputa
 		end = time.time()
 		totalktime += end - start
 		it += 1
 	print("Total GDL Kernel Time:", totalktime, "seconds")
-	return output
+	return embed
 
 #manual result verification
 def gsddmmspmmkerneltest(graph, A):
+	sm_table = init_SM_TABLE()
 	coo = graph.adj()._indices().numpy()
 	N = graph.adj().size()[0]
 	S = [[0 for i in range(N)] for j in range(N)]
@@ -167,8 +215,9 @@ def gsddmmspmmkerneltest(graph, A):
 		X[u][v] = dp
 	#X = np.matmul(np.matmul(A, B), S)
 	#print(X)
-	Y = 1.0 / (1.0 + np.exp(-X))
-	Z = np.array([[0 if S[i][j] == 0 else 1-Y[i][j] for j in range(N)] for i in range(N)])
+	#Y = 1.0 / (1.0 + np.exp(-X))
+	#Z = np.array([[0 if S[i][j] == 0 else 1-Y[i][j] for j in range(N)] for i in range(N)])
+	Z = allscale(X)
 	#print(Z)
 	O = np.matmul(Z, A)
 	return O
@@ -203,7 +252,7 @@ if __name__ == "__main__":
 	parser.add_argument('-t', '--t', required=False, type=int, default=1, help='Similarity function type, default:1(sigmoid)')
 	parser.add_argument('-r', '--r', required=False, type=float, default=1.0, help='Learning rate, default:1.0')
 	parser.add_argument('-it', '--it', required=False, type=int, default=1, help='Iterations, default:1')
-	parser.add_argument('-b', '--b', required=False, type=int, default=64, help='Batch Size, default:64')
+	parser.add_argument('-b', '--b', required=False, type=int, default=256, help='Batch Size, default:256')
 	parser.add_argument('-p', '--p', required=False, type=str, default="", help='Path to mtx graph format, default:None')
 
 	args = parser.parse_args()
@@ -235,12 +284,14 @@ if __name__ == "__main__":
 	embed = torch.rand(N, dim)
 	#print(embed)
 	#need to check batch processing ...
+	print("Creating batch graphs...")
 	bgraphs = batch_process(graph, bsize)
+	print("Done!")
 	#cacheflush()
-	f2voutput = f2vfunctions(bgraphs, embed, ftype, it, lrate)
+	f2voutput = f2vfunctions(bgraphs, embed.clone(), ftype, it, lrate)
 	print("Fused SDDMM+SPMM Kernel:")
 	print(f2voutput)
-	dgloutput = dglfunctions(bgraphs, embed, ftype, it, lrate)
+	dgloutput = dglfunctions(bgraphs, embed.clone(), ftype, it, lrate)
 	print("DGL: SDDMMSPMM+Transformation+SPMM")
 	print(dgloutput)
 	#out = gsddmmspmmkerneltest(graph, embed)
